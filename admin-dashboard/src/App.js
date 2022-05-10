d8de3546a51f1dcac7f14a2cef0293b3f330e950
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-05-10 23:43:05 +0530

    completed the sidebar

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,9 +1,15 @@
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others"></div>
+      </div>
     </div>
   );
 }
