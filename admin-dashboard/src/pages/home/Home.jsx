10313a326eb34b87ced013abf3e51162388de84b
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-05-12 23:45:32 +0530

    small & large widjets

--- admin-dashboard/src/pages/home/Home.jsx
+++ admin-dashboard/src/pages/home/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-// import Charts from '../../components/charts/Charts'
+import Charts from '../../components/charts/Charts'
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import WidgetLg from '../../components/widgetLg/WidgetLg'
+import WidgetSm from '../../components/widgetSm/WidgetSm'
 import './home.css'
 
 export default function Home() {
     return (
         <div className='home'>
             <FeaturedInfo />
-            {/* <Charts /> */}
+            <Charts />
+            <div className="homeWidgets">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
 }
