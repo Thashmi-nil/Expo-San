1320af59c4f875dfb231cbe5c698833729515180
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-09-07 00:50:52 +0530

    products

---
+++ admin-dashboard/src/pages/productList/ProductList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './productList.css'
+
+export default function ProductList() {
+    return (
+        <div className='productList'>
+            ProductList
+        </div>
+    )
+}
