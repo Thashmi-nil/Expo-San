af54aa5d55ba4d258a394ee1e1a38f1999f9b18b
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-09-07 00:49:48 +0530

    system logs & transaction

---
+++ admin-dashboard/src/pages/systemLog/SystemLog.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './systemLog.css'
+
+export default function SystemLog() {
+    return (
+        <div className='systemLog'>SystemLog</div>
+    )
+}
---
+++ admin-dashboard/src/pages/transactions/Transaction.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './transactions.css'
+
+export default function Transaction() {
+    return (
+        <div className='transactions'>Transaction</div>
+    )
+}
