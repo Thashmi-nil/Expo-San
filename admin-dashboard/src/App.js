5d57cda4bf94465d7be3c320d4c274d0455303b8
Nilupuli Sanjana <nilupuleethash765@gmail.com>
2022-09-07 00:47:37 +0530

    add other files

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -29,7 +29,16 @@ import {
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
-
+import ProductList from "./pages/productList/ProductList";
+import Analytics from "./pages/analytics/Analytics";
+import Sales from "./pages/sales/Sales";
+import Transactions from "./pages/transactions/Transaction";
+import Reports from "./pages/reports/Reports";
+import Malils from "./pages/mails/Mails";
+import Feedback from "./pages/feedback/Feedback";
+import Messages from "./pages/messages/Messages";
+import Manage from "./pages/manage/Manage";
+import SystemLog from "./pages/systemLog/SystemLog";
 
 function App() {
   return (
@@ -41,6 +50,12 @@ function App() {
           <Route exact path="/">
             <Home />
           </Route>
+          <Route path="/analytics">
+            <Analytics />
+          </Route>
+          <Route path="/sales">
+            <Sales />
+          </Route>
           <Route path="/users">
             <UserList />
           </Route>
@@ -49,6 +64,36 @@ function App() {
           </Route>
           <Route path="/newUser">
             <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/products/:productId">
+            <User />
+          </Route>
+          <Route path="/newProduct">
+            <NewUser />
+          </Route>
+          <Route path="/transactions">
+            <Transactions />
+          </Route>
+          <Route path="/reports">
+            <Reports />
+          </Route>
+          <Route path="/mails">
+            <Malils />
+          </Route>
+          <Route path="/feedback">
+            <Feedback />
+          </Route>
+          <Route path="/messages">
+            <Messages />
+          </Route>
+          <Route path="/manage">
+            <Manage />
+          </Route>
+          <Route path="/systemLog">
+            <SystemLog />
           </Route>
         </Switch>
       </div>
